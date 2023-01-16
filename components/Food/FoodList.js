18432a42fcbd30c2bfbcc92313633a980561c256
merlemak <merlemakoben@gmail.com>
2023-01-16 16:06:56 +0000

    Add filter function by category and month

--- components/Food/FoodList.js
+++ components/Food/FoodList.js
@@ -1,14 +1,58 @@
-import styled from "styled-components";
-import { useState } from "react";
-import { Fragment } from "react";
-import FoodCard from "./FoodCard";
-import Image from "next/image";
-import SearchIcon from "../../assets/images/search-icon.svg";
+import styled from 'styled-components';
+import { useState } from 'react';
+import { Fragment } from 'react';
+import FoodCard from './FoodCard';
+import Image from 'next/image';
+import SearchIcon from '../../assets/images/search-icon.svg';
 
 export default function FoodList({ onToggleFavorite, foods }) {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
+
+  const [categoryFilter, setCategoryFilter] = useState('All');
+  const [monthFilter, setMonthFilter] = useState('All');
+
+  // categoryFilter = 'Gemüse'
+  let foodsToDisplay = [...foods];
+
+  foodsToDisplay.sort((foodA, foodB) => {
+    if (foodA.name > foodB.name) return 1;
+    if (foodA.name < foodB.name) return -1;
+    return 0;
+  });
+
+  if (categoryFilter !== 'All') {
+    foodsToDisplay = foods.filter((food) => food.category === categoryFilter);
+  }
+
+  // seasonFilter = 'August'
+  if (monthFilter !== 'All') {
+    foodsToDisplay = foodsToDisplay.filter((food) =>
+      food.availability.includes(monthFilter)
+    );
+  }
+
   return (
     <>
+      <div>
+        <button onClick={() => setCategoryFilter('All')}>Alle</button>
+        <button onClick={() => setCategoryFilter('Gemüse')}>Gemüse</button>
+        <button onClick={() => setCategoryFilter('Obst')}>Obst</button>
+      </div>
+      <div>
+        <button onClick={() => setMonthFilter('All')}>Zurücksetzen</button>
+        <button onClick={() => setMonthFilter('Januar')}>Januar</button>
+        <button onClick={() => setMonthFilter('Februar')}>Februar</button>
+        <button onClick={() => setMonthFilter('März')}>März</button>
+        <button onClick={() => setMonthFilter('April')}>April</button>
+        <button onClick={() => setMonthFilter('Mai')}>Mai</button>
+        <button onClick={() => setMonthFilter('Juni')}>Juni</button>
+        <button onClick={() => setMonthFilter('Juli')}>Juli</button>
+        <button onClick={() => setMonthFilter('August')}>August</button>
+        <button onClick={() => setMonthFilter('September')}>September</button>
+        <button onClick={() => setMonthFilter('Oktober')}>Oktober</button>
+        <button onClick={() => setMonthFilter('November')}>November</button>
+        <button onClick={() => setMonthFilter('Dezember')}>Dezember</button>
+      </div>
       <StyledHomeHeader>Dezember</StyledHomeHeader>
       <StyledSearchInput>
         <StyledSearchIcon
@@ -27,9 +71,9 @@ export default function FoodList({ onToggleFavorite, foods }) {
       <StyledFoodListHeader>Obst & Gemüse</StyledFoodListHeader>
 
       <StyledFoodList>
-        {foods
+        {foodsToDisplay
           .filter((food) => {
-            return search.toLowerCase() === ""
+            return search.toLowerCase() === ''
               ? food
               : food.name.toLowerCase().startsWith(search);
           })
